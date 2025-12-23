451e82e5ad7f8c96c533292bfea3f95e0be924b8
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-24 01:17:09 +0700

    method --> function yang ada di class kalo di typescript harus menentukan tipe data di parameter dan return valuenya

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -9,6 +9,10 @@ describe("Properties", () => {
       this.id = id;
       this.name = name;
     }
+
+    sayHello(name: string): void {
+      console.info(`Hello ${name}, my name is ${this.name}`);
+    }
   }
 
   it("should can have properties", () => {
@@ -19,5 +23,10 @@ describe("Properties", () => {
     console.info(costumer.id);
     console.info(costumer.name);
     console.info(costumer.age);
+  });
+
+  it("shoul can have methoed", () => {
+    const costumer: Costumer = new Costumer("J0404231072", "Ghina");
+    costumer.sayHello("Bonardo");
   });
 });
