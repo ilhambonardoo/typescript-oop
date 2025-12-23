04a8c13b8458dff7000c8a7fc85956b6c29044b8
ilhambonardoo <ilhambonardo123@gmail.com>
2025-12-24 00:50:10 +0700

    constructor --> method atau function yang dipanggil ketika memiliki object dibuat dari class.

--- test/class.test.ts
+++ test/class.test.ts
@@ -1,10 +1,19 @@
 describe("class", () => {
+  class Costumer {
+    constructor() {
+      console.info("Create new costumer");
+    }
+  }
+
+  class Order {}
+
   it("should can create class", () => {
-    class Costumer {}
-
-    class Order {}
-
     const costumer: Costumer = new Costumer();
     const order = new Order();
+  });
+
+  it("should can create constructor", () => {
+    new Costumer();
+    new Costumer();
   });
 });
